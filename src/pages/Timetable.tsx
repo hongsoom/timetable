ab820c4958e0b9b651a6e719510bc99a9e5b1ede
hongsoom <hongsoom@kakao.com>
2023-02-19 22:36:05 +0900

    [Feat] 시간표 전체 컴포넌트

---
+++ src/pages/Timetable.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from "react";
+import Main from "../../components/main/Main";
+import { defaultSemester } from "../../components/constant/semeter";
+
+export const MajorContext: React.Context<any> = createContext(null);
+export const SemesterContext: React.Context<any> = createContext(null);
+
+const Timetable = () => {
+    const [major, setMajor] = useState(null);
+    const [semester, setSemester] = useState(defaultSemester.value);
+
+    return (
+        <MajorContext.Provider value={{ major, setMajor }}>
+            <SemesterContext.Provider value={{ semester, setSemester }}>
+                <div className="flex lg:h-auto p-4 md:p-10 bg-purple-50">
+                    <div className="bg-white w-full p-5 md:p-10 rounded-2xl h-full">
+                        <h1 className="text-2xl md:text-3xl p-3 md:p-5 border-b-2 border-stone-900 mb-5">
+                            🗓 시간표 생성기
+                        </h1>
+                        <Main />
+                    </div>
+                </div>
+            </SemesterContext.Provider>
+        </MajorContext.Provider>
+    );
+};
+
+export default Timetable;
