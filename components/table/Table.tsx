8613ee67e449f335a8fc23174eb9bbf25b29b035
hongsoom <hongsoom@kakao.com>
2023-03-18 23:40:19 +0900

    [Feat] Table 컴포넌트 생성

---
+++ components/table/Table.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Table = () => {
+  return (
+    <>
+      <div className='flex flex-col lg:flex-row w-full justify-between'>
+        <p>테이블 컴포넌트</p>
+      </div>
+    </>
+  );
+};
+
+export default Table;
--- components/table/TableContainer.tsx
+++ components/table/TableContainer.tsx
@@ -1,12 +1,13 @@
 import React, { createContext, useState } from 'react';
 import Serach from './Serach';
+import Table from './Table';
 
 const TableContainer = () => {
   return (
     <>
       <div className='flex flex-col lg:flex-row w-full justify-between'>
         <Serach />
-        <p>시간표 테이블</p>
+        <Table />
       </div>
       <div>
         <p>장바구니</p>
