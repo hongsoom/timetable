0f7c1b386b7757bd3c26bf6cb12edaf15b8882d7
SUMIN HONG <99132215+hongsoom@users.noreply.github.com>
2023-03-20 23:50:49 +0900

    [Feat] 테이블 형성

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -4,7 +4,20 @@ const Table = () => {
   return (
     <>
       <div className='flex flex-col lg:flex-row w-full justify-between'>
-        <p>테이블 컴포넌트</p>
+        <TableHeaderBox>
+          <tr>
+            <HeaderColumn id='id' className='sort'>
+              주문번호 ▲
+            </HeaderColumn>
+            <HeaderColumn>고객번호</HeaderColumn>
+            <HeaderColumn>고객명</HeaderColumn>
+            <HeaderColumn>가격</HeaderColumn>
+            <HeaderColumn id='transaction_time' className='sort'>
+              거래일 & 거래시간 ▲
+            </HeaderColumn>
+            <HeaderColumn>주문 처리 상태</HeaderColumn>
+          </tr>
+        </TableHeaderBox>
       </div>
     </>
   );
